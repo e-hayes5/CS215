6f800cdc7c1af8f0b1da89f449c3303fa866692c
e-hayes5 <ehayes5@bellarmine.edu>
2026-03-10 15:24:22 -0400

    Add files via upload

---
+++ CS215_BSTLab1.java
@@ -0,0 +1,76 @@
+
+public class BST {
+	private class TNode{
+		public int item;
+		TNode left;
+		TNode right;
+		public TNode(int newitem) {
+			this.item = newitem;
+			left = null;
+			right = null;
+		}
+	}
+	TNode root;
+	public BST() {
+		//initialize root to be NULL
+		root = null;
+	}
+	
+	private TNode insert(TNode curnode, int newitem) {
+		if (curnode == null) {
+			curnode = new TNode(newitem);
+		}
+		else if (newitem<curnode.item) {
+			curnode.left = insert(curnode.left, newitem);
+		}
+		else if (newitem>curnode.item) {
+			curnode.right = insert(curnode.right, newitem);
+		}
+		return curnode;
+	}
+	
+	public void putItem(int newitem) {
+		root = insert(root, newitem);
+	}
+	
+	public void printNodes(TNode curnode) {
+		if(curnode != null) {
+			printNodes(curnode.left);
+			System.out.print(curnode.item+ " ");
+			printNodes(curnode.right);
+		}
+		
+	}
+	
+	public boolean isEmpty() {
+		return root==null;
+	}
+	
+	public void printTree() {
+		if (isEmpty()) {
+			System.out.println("Empty Tree");
+		}
+		else {
+			printNodes(root);
+		}
+	}
+	
+	public int getItem(int gitem) {
+		return findItem(root, gitem);
+	}
+	
+	private int findItem(TNode curnode, int gitem) {
+		if(curnode == null) {
+			return -1;
+		}
+		else if(curnode.item == gitem) {
+			return curnode.item;
+		}
+		else if(gitem < curnode.item) {
+			return findItem(curnode.left, gitem);
+		}
+		else {
+			return findItem(curnode.right, gitem);
+		}
+	}
+}
